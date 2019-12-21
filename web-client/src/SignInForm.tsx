5653caffa793a0b9b3585d960257d64c12793e19
Matt Westrik <m@ttwestrik.com>
2019-12-21 05:48:42 -0800

    add change/click handlers for sign-in form

--- web-client/src/SignInForm.tsx
+++ web-client/src/SignInForm.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 
 import './style/SignInForm.scss';
 
@@ -6,47 +7,66 @@ interface IProps {
   onSignIn: () => void;
 }
 
-const SignInForm: React.FC<IProps> = props => (
-  <div className="form-container text-center">
-    <form className="form-signin">
-      <h1 className="h3 mb-3 font-weight-normal">
-        Sign in to <em>Timeline</em>
-      </h1>
-      <label htmlFor="inputEmail" className="sr-only">
-        Email address
-      </label>
-      <input
-        type="email"
-        id="inputEmail"
-        className="form-control"
-        placeholder="Email address"
-        required
-        autoFocus
-      />
-      <label htmlFor="inputPassword" className="sr-only">
-        Password
-      </label>
-      <input
-        type="password"
-        id="inputPassword"
-        className="form-control"
-        placeholder="Password"
-        required
-      />
-      <div className="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me" /> Remember me
+const SignInForm: React.FC<IProps> = props => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [remember, setRemember] = useState(false);
+
+  return (
+    <div className="form-container text-center">
+      <form className="form-signin">
+        <h1 className="h3 mb-3 font-weight-normal">
+          Sign in to <em>Timeline</em>
+        </h1>
+        <label htmlFor="inputEmail" className="sr-only">
+          Email address
         </label>
-      </div>
-      <button
-        onClick={props.onSignIn}
-        className="btn btn-lg btn-primary btn-block"
-        type="submit"
-      >
-        Sign in
-      </button>
-    </form>
-  </div>
-);
+        <input
+          type="email"
+          id="inputEmail"
+          className="form-control"
+          placeholder="Email address"
+          required
+          autoFocus
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+        />
+        <label htmlFor="inputPassword" className="sr-only">
+          Password
+        </label>
+        <input
+          type="password"
+          id="inputPassword"
+          className="form-control"
+          placeholder="Password"
+          required
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
+        />
+        <div className="checkbox mb-3">
+          <label>
+            <input
+              type="checkbox"
+              value="remember-me"
+              onClick={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setRemember(e.target.checked)
+              }
+            />
+            Remember me
+          </label>
+        </div>
+        <button
+          onClick={props.onSignIn}
+          className="btn btn-lg btn-primary btn-block"
+          type="submit"
+        >
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default SignInForm;
