90a6210656b4f100571fac26b543354b1c130534
Matt Westrik <m@ttwestrik.com>
2020-01-03 21:26:28 -0800

    cleanup

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,6 +1,5 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-import { render } from 'react-dom';
+import { h, render } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 import Dashboard from './Dashboard';
 import SignInForm, { Session, User } from './SignInForm';
 
@@ -9,65 +8,57 @@ const TOKEN_KEY = 'access_token';
 const EXPIRATION_KEY = 'access_expiration';
 
 const API_HOSTS = {
-  local: 'http://api.westrik.world:6874',
-  production: 'https://api.westrikworld.com',
-  staging: 'https://api.staging.westrikworld.com',
+    local: 'http://api.westrik.world:6874',
+    production: 'https://api.westrikworld.com',
+    staging: 'https://api.staging.westrikworld.com',
 };
 const env = process.env.NODE_ENV;
 export const API_HOST =
-  env === 'staging'
-    ? API_HOSTS.staging
-    : env === 'production'
-      ? API_HOSTS.production
-      : API_HOSTS.local;
+    env === 'staging' ? API_HOSTS.staging : env === 'production' ? API_HOSTS.production : API_HOSTS.local;
 
-const App: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-  const [bearerToken, setBearerToken] = useState(
-    sessionStorage.getItem(TOKEN_KEY) || localStorage.getItem(TOKEN_KEY)
-  );
-  useEffect(() => {
+function App(): h.JSX.Element | null {
+    const [loading, setLoading] = useState(true);
+    const [bearerToken, setBearerToken] = useState(
+        sessionStorage.getItem(TOKEN_KEY) || localStorage.getItem(TOKEN_KEY),
+    );
+    useEffect(() => {
+        if (loading) {
+            setLoading(false);
+        }
+    });
     if (loading) {
-      setLoading(false);
+        return null;
+    } else if (bearerToken) {
+        return (
+            <Dashboard
+                {...SITE_PROPS}
+                apiToken={bearerToken}
+                onSignOut={(): void => {
+                    setBearerToken('');
+                    sessionStorage.clear();
+                    localStorage.clear();
+                }}
+            />
+        );
+    } else {
+        return (
+            <SignInForm
+                {...SITE_PROPS}
+                onSignIn={(persistLogin: boolean, user: User, session: Session): void => {
+                    setBearerToken(session.token);
+                    const storage = persistLogin ? localStorage : sessionStorage;
+                    storage.setItem(TOKEN_KEY, session.token);
+                    storage.setItem(EXPIRATION_KEY, session.expires_at);
+                }}
+            />
+        );
     }
-  });
-  if (loading) {
-    return null;
-  } else if (bearerToken) {
-    return (
-      <Dashboard
-        {...SITE_PROPS}
-        apiToken={bearerToken}
-        onSignOut={(): void => {
-          setBearerToken('');
-          sessionStorage.clear();
-          localStorage.clear();
-        }}
-      />
-    );
-  } else {
-    return (
-      <SignInForm
-        {...SITE_PROPS}
-        onSignIn={(
-          persistLogin: boolean,
-          user: User,
-          session: Session
-        ): void => {
-          setBearerToken(session.token);
-          const storage = persistLogin ? localStorage : sessionStorage;
-          storage.setItem(TOKEN_KEY, session.token);
-          storage.setItem(EXPIRATION_KEY, session.expires_at);
-        }}
-      />
-    );
-  }
-};
+}
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root')!);
 
 // @ts-ignore
 if (module.hot) {
-  // @ts-ignore
-  module.hot.accept();
+    // @ts-ignore
+    module.hot.accept();
 }
--- web-client/tests/base/test.ts
+++ web-client/tests/base/test.ts
@@ -2,7 +2,7 @@ import { assert } from 'chai';
 import 'mocha';
 
 describe('app', () => {
-  it('should run this test', () => {
-    assert.equal([1, 2, 3].indexOf(4), -1);
-  });
+    it('should run this test', () => {
+        assert.equal([1, 2, 3].indexOf(4), -1);
+    });
 });
