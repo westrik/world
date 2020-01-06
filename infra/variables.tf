f74ea5556b06e05b6b117f338ffb2798b5f6efa1
Matt Westrik <m@ttwestrik.com>
2020-01-05 18:01:38 -0800

    tf cleanup & refactor

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,15 @@
 variable "aws_region" {
   default = "us-east-1"
 }
+
+variable "aws_az" {
+  default = "us-east-1a"
+}
+
+variable "api_domain_name" {
+  default = "api.westrikworld.com"
+}
+
+variable "frontend_domain_name" {
+  default = "westrikworld.com"
+}
