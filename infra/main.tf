0aa3f867eba370c0d8a612f544899b9470f9be8e
Matt Westrik <m@ttwestrik.com>
2020-01-05 17:53:22 -0800

    provision EC2 instance w/ AMI; clean up AMI tagging

--- infra/main.tf
+++ infra/main.tf
@@ -83,7 +83,49 @@ resource "aws_subnet" "ww_prod_app" {
   cidr_block              = "10.0.1.0/24"
 //  map_public_ip_on_launch = true
   // (don't think we'll need public IPs since we're using an NLB)
+
+  tags = {
+    "Name" = "ww_prod_app",
+  }
 }
+
+data "aws_ami" "westrikworld" {
+  most_recent      = true
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["westrikworld *"]
+  }
+  filter {
+    name   = "tag:Environment"
+    values = ["production"]
+  }
+  filter {
+    name   = "tag:OS_Version"
+    values = ["Debian 10"]
+  }
+}
+
+resource "aws_instance" "ww_prod_app" {
+  instance_type = "t3a.micro"
+  ami = "${data.aws_ami.westrikworld.id}"
+  vpc_security_group_ids = ["${aws_security_group.ww_prod_app.id}"]
+  subnet_id = "${aws_subnet.ww_prod_app.id}"
+
+  # TODO: change default login and SSH config for AMI (no password)
+  # TODO: configure with a stored keypair to allow login via bastion
+
+  tags {
+    Name = "ww_prod_app_instance",
+    Environment = "production"
+  }
+}
+
+//resource "aws_eip" "lb" {
+//  instance = "${aws_instance.web.id}"
+//  vpc      = true
+//}
 
 
 
@@ -94,8 +136,6 @@ resource "aws_subnet" "ww_prod_app" {
 
 resource "aws_vpc" "packer_build" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
 
   tags = {
     Name = "packer_build"
