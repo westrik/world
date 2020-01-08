ba76c50dcd120a61c2ea55d655f75187814323ad
Matt Westrik <m@ttwestrik.com>
2020-01-08 01:51:38 -0800

    create cloudfront distribution (no R53 / ACM cert)

--- infra/modules/deploy/main.tf
+++ infra/modules/deploy/main.tf
@@ -222,3 +222,63 @@ resource "aws_codepipeline" "app" {
     }
   }
 }
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.app_deploy_cloudfront.bucket_regional_domain_name
+    origin_id   = "${var.project_name}_prod"
+
+//    s3_origin_config {
+//      origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567" // TODO: replace
+//    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+//  comment             = ""
+  default_root_object = "index.html"
+
+//  logging_config {
+//    include_cookies = false
+//    bucket          = "mylogs.s3.amazonaws.com"
+//    prefix          = "myprefix"
+//  }
+
+//  aliases = ["mysite.example.com", "yoursite.example.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${var.project_name}_prod"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA"]
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
