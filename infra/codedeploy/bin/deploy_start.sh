d90e2e5c1441912a593fae10ac0386771545ade8
Matt Westrik <m@ttwestrik.com>
2020-01-11 22:30:58 -0800

    fix permissions + folder structure

--- infra/codedeploy/bin/deploy_start.sh
+++ infra/codedeploy/bin/deploy_start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SERVICE_D="/etc/systemd/system/app.service.d"
+SERVICE_CONF="/etc/systemd/system/app.service"
+SERVICE_D="$SERVICE_CONF.d"
 SECRETS_ENV_FILE="$SERVICE_D/production.conf"
 SERVER_BIN_FILE=/usr/bin/run_server
 set -euxo pipefail
@@ -16,15 +17,20 @@ fi
 mkdir -p $SERVICE_D
 touch $SECRETS_ENV_FILE
 chown root:root $SECRETS_ENV_FILE
-chmod 700 $SECRETS_ENV_FILE
-# TODO: run aws cli to pull db url and pw salt from secrets manager
+chmod 644 $SECRETS_ENV_FILE
+
+chown root:root $SERVICE_CONF
+chmod 644 $SERVICE_CONF
+
 aws configure set region us-east-1
 DATABASE_URL=$(aws secretsmanager get-secret-value --secret-id "database_url" | jq '.SecretString')
 PASSWORD_HASH_SALT=$(aws secretsmanager get-secret-value --secret-id "password_hash_salt" | jq '.SecretString')
 echo "[Service]" >> $SECRETS_ENV_FILE
-echo "Environment=\"DATABASE_URL=$DATABASE_URL\"" >> $SECRETS_ENV_FILE
-echo "Environment=\"PASSWORD_HASH_SALT=$PASSWORD_HASH_SALT\"" >> $SECRETS_ENV_FILE
+echo "Environment='DATABASE_URL=$DATABASE_URL'" >> $SECRETS_ENV_FILE
+echo "Environment='PASSWORD_HASH_SALT=$PASSWORD_HASH_SALT'" >> $SECRETS_ENV_FILE
 systemctl daemon-reload
 
+chown root:root $SERVER_BIN_FILE
+chmod 744 $SERVER_BIN_FILE
 chmod +x $SERVER_BIN_FILE
 systemctl start app
