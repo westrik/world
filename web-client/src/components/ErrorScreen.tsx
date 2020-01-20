568ab2fc189972624d460410e3fa167b29aa52a5
Matt Westrik <m@ttwestrik.com>
2020-01-20 05:31:20 -0800

    style 404 component

--- web-client/src/components/ErrorScreen.tsx
+++ web-client/src/components/ErrorScreen.tsx
@@ -5,5 +5,14 @@ interface Props {
 }
 
 export default function ErrorScreen(props: Props): h.JSX.Element {
-    return <h1>{props.error || '404'}</h1>;
+    return (
+        <div className="d-flex vh-100 align-items-center">
+            <div className="jumbotron container" style="max-width: 500px">
+                <h1 className="display-4">{props.error || '404'}</h1>
+                <p className="lead">
+                    <a href="/">go home</a>
+                </p>
+            </div>
+        </div>
+    );
 }
