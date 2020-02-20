c7d94857e54e95aa4f43c7d9fb4476d070702151
Matt Westrik <m@ttwestrik.com>
2020-02-19 23:29:26 -0800

    fix github actions build?

--- .github/scripts/test_db.sh
+++ .github/scripts/test_db.sh
@@ -1,5 +1,6 @@
 set -euxo pipefail
 
+sudo apt-get update
 sudo apt-get install libpq-dev postgresql postgresql-client
 sudo sed -i 's/local   all             postgres                                peer/host    all             all             127.0.0.1\/32            trust/g' /etc/postgresql/10/main/pg_hba.conf
 sudo service postgresql restart
