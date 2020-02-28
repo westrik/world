3a4feebbf18c2d1f261e7c6997af9abc85cf0eac
Matt Westrik <m@ttwestrik.com>
2020-02-27 18:39:49 -0800

    stub out Event and Tag structs

--- server/src/notes/parsing.rs
+++ server/src/notes/parsing.rs
@@ -1,23 +1,60 @@
 use pulldown_cmark::{html, Options, Parser};
 
 #[derive(Deserialize, Serialize)]
-pub struct Content {}
+pub struct Content {
+    events: Vec<Event>,
+}
 
-pub fn parse_markdown(input: String) -> Content {
+#[derive(Deserialize, Serialize)]
+pub enum Event {
+    Start(Tag),
+    End(Tag),
+    Text(String),
+    Code(String),
+    Html(String),
+    FootnoteReference(String),
+    SoftBreak,
+    HardBreak,
+    Rule,
+    TaskListMarker(bool),
+}
+
+#[derive(Deserialize, Serialize)]
+pub enum Tag {
+    Paragraph,
+    Heading(u32),
+    BlockQuote,
+    CodeBlock, // TODO
+    List(Option<u64>),
+    Item,
+    FootnoteDefinition(String),
+    Table, // TODO
+    TableHead,
+    TableRow,
+    TableCell,
+    Emphasis,
+    Strong,
+    Strikethrough,
+    Link,  // TODO
+    Image, // TODO
+}
+
+fn get_parser_options() -> Options {
     let mut parser_opts = Options::empty();
     parser_opts.insert(Options::ENABLE_TABLES);
     parser_opts.insert(Options::ENABLE_FOOTNOTES);
     parser_opts.insert(Options::ENABLE_STRIKETHROUGH);
     parser_opts.insert(Options::ENABLE_TASKLISTS);
+    parser_opts
+}
 
-    let parser = Parser::new_ext(input.as_str(), parser_opts);
+pub fn markdown_to_html(input: String) -> String {
+    let parser = Parser::new_ext(input.as_str(), get_parser_options());
 
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
 
-    println!("{}", html_output);
-
-    Content {}
+    html_output
 }
 
 #[cfg(test)]
@@ -26,8 +63,20 @@ pub mod test_resource_identifiers {
 
     #[test]
     fn test_parsing() {
-        parse_markdown("# Hello world".to_string());
-        parse_markdown("~~Hello world~~".to_string());
-        parse_markdown("- [ ] hello\n- [ ] world".to_string());
+        assert_eq!(
+            markdown_to_html("# Hello world".to_string()),
+            "<h1>Hello world</h1>\n"
+        );
+        assert_eq!(
+            markdown_to_html("~~Hello world~~".to_string()),
+            "<p><del>Hello world</del></p>\n"
+        );
+        assert_eq!(
+            markdown_to_html("- [ ] hello\n- [ ] world".to_string()),
+            "<ul>\n\
+                <li><input disabled=\"\" type=\"checkbox\"/>\nhello</li>\n\
+                <li><input disabled=\"\" type=\"checkbox\"/>\nworld</li>\n\
+            </ul>\n"
+        );
     }
 }
