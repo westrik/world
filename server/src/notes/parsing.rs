db7429439c6a8fc2263052f98a35db9ed388ae88
Matt Westrik <m@ttwestrik.com>
2020-02-27 18:24:07 -0800

    enable footnotes, tables, and task-lists

--- server/src/notes/parsing.rs
+++ server/src/notes/parsing.rs
@@ -4,9 +4,13 @@ use pulldown_cmark::{html, Options, Parser};
 pub struct Content {}
 
 pub fn parse_markdown(input: String) -> Content {
-    let mut options = Options::empty();
-    options.insert(Options::ENABLE_STRIKETHROUGH);
-    let parser = Parser::new_ext(input.as_str(), options);
+    let mut parser_opts = Options::empty();
+    parser_opts.insert(Options::ENABLE_TABLES);
+    parser_opts.insert(Options::ENABLE_FOOTNOTES);
+    parser_opts.insert(Options::ENABLE_STRIKETHROUGH);
+    parser_opts.insert(Options::ENABLE_TASKLISTS);
+
+    let parser = Parser::new_ext(input.as_str(), parser_opts);
 
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
@@ -24,5 +28,6 @@ pub mod test_resource_identifiers {
     fn test_parsing() {
         parse_markdown("# Hello world".to_string());
         parse_markdown("~~Hello world~~".to_string());
+        parse_markdown("- [ ] hello\n- [ ] world".to_string());
     }
 }
