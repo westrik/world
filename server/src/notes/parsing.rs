33c2096d5f5ab67cca1959ad03c2952fb639c6b3
Matt Westrik <m@ttwestrik.com>
2020-02-27 18:21:12 -0800

    md -> html with pulldown_cmark

--- server/src/notes/parsing.rs
+++ server/src/notes/parsing.rs
@@ -1,1 +1,28 @@
+use pulldown_cmark::{html, Options, Parser};
 
+#[derive(Deserialize, Serialize)]
+pub struct Content {}
+
+pub fn parse_markdown(input: String) -> Content {
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    let parser = Parser::new_ext(input.as_str(), options);
+
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+
+    println!("{}", html_output);
+
+    Content {}
+}
+
+#[cfg(test)]
+pub mod test_resource_identifiers {
+    use super::*;
+
+    #[test]
+    fn test_parsing() {
+        parse_markdown("# Hello world".to_string());
+        parse_markdown("~~Hello world~~".to_string());
+    }
+}
