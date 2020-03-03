12d58cfeeb884d1018210d1ea048c771de25de5e
Matt Westrik <m@ttwestrik.com>
2020-03-03 00:25:56 -0800

    [be-test] test runner should flush stdout

--- server/integration_tests/src/test_runner.rs
+++ server/integration_tests/src/test_runner.rs
@@ -1,17 +1,21 @@
 use crate::db::{create_test_db, destroy_test_db, rollback_txn, start_txn, DbPool};
+use std::io::{stdout, Write};
 
 #[cfg(test)]
 pub fn runner(tests: &[&dyn Fn(&DbPool)]) {
     println!("⚙️  setting up environment for integration tests...");
     let pool = create_test_db();
 
-    println!("\n📋 running {} integration tests\n", tests.len());
+    print!("\n📋 running {} integration tests: ", tests.len());
+    stdout().flush().unwrap();
+
     for test in tests {
         let pool = pool.clone();
         start_txn(&pool);
         test(&pool);
         rollback_txn(&pool);
         print!(".");
+        stdout().flush().unwrap();
     }
     print!("\n\n");
     destroy_test_db(&pool);
