8a87cd21ae35885b4b02fcfb8b8d288fc12b5362
Matt Westrik <m@ttwestrik.com>
2020-03-03 20:19:31 -0800

    [be] fully implement md parsing + tests

--- server/core/src/notes/content_schema.rs
+++ server/core/src/notes/content_schema.rs
@@ -24,17 +24,41 @@ pub enum Tag {
     Paragraph,
     Heading(u32),
     BlockQuote,
-    CodeBlock, // TODO
+    CodeBlock(Option<String>),
     List(Option<u64>),
     Item,
     FootnoteDefinition(String),
-    Table, // TODO
+    Table(Vec<ColumnType>),
     TableHead,
     TableRow,
     TableCell,
     Emphasis,
     Strong,
     Strikethrough,
-    Link,  // TODO
-    Image, // TODO
+    Link(LinkType, String, String),
+    Image(LinkType, String, String),
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+pub enum LinkType {
+    // Inline link like `[foo](bar)`
+    Inline,
+    // Reference link like `[foo][bar]`
+    Reference,
+    // Collapsed link like `[foo][]`
+    Collapsed,
+    // Shortcut link like `[foo]`
+    Shortcut,
+    // Autolink like `<http://foo.bar/baz>`
+    Autolink,
+    // Email address in autolink like `<john@example.org>`
+    Email,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+pub enum ColumnType {
+    None,
+    Left,
+    Center,
+    Right,
 }
--- server/core/src/notes/parsing.rs
+++ server/core/src/notes/parsing.rs
@@ -1,5 +1,8 @@
-use crate::notes::content_schema::{Event, Tag};
-use pulldown_cmark::{html, Event as ParserEvent, Options, Parser, Tag as ParserTag};
+use crate::notes::content_schema::{ColumnType, Event, LinkType, Tag};
+use pulldown_cmark::{
+    html, Alignment, CodeBlockKind, Event as ParserEvent, LinkType as ParserLinkType, Options,
+    Parser, Tag as ParserTag,
+};
 
 fn get_parser_options() -> Options {
     let mut parser_opts = Options::empty();
@@ -39,19 +42,54 @@ fn transform_parse_tag(tag: ParserTag) -> Tag {
         ParserTag::Paragraph => Tag::Paragraph,
         ParserTag::Heading(size) => Tag::Heading(size),
         ParserTag::BlockQuote => Tag::BlockQuote,
-        ParserTag::CodeBlock(_) => Tag::CodeBlock,
+        ParserTag::CodeBlock(kind) => match kind {
+            CodeBlockKind::Indented => Tag::CodeBlock(None),
+            CodeBlockKind::Fenced(lang) => Tag::CodeBlock(Some(lang.into_string())),
+        },
         ParserTag::List(length) => Tag::List(length),
         ParserTag::Item => Tag::Item,
         ParserTag::FootnoteDefinition(fdef) => Tag::FootnoteDefinition(fdef.into_string()),
-        ParserTag::Table(_) => Tag::Table,
+        ParserTag::Table(alignments) => Tag::Table(
+            alignments
+                .iter()
+                .map(|alignment| match *alignment {
+                    Alignment::None => ColumnType::None,
+                    Alignment::Left => ColumnType::Left,
+                    Alignment::Center => ColumnType::Center,
+                    Alignment::Right => ColumnType::Right,
+                })
+                .collect(),
+        ),
         ParserTag::TableHead => Tag::TableHead,
         ParserTag::TableRow => Tag::TableRow,
         ParserTag::TableCell => Tag::TableCell,
         ParserTag::Emphasis => Tag::Emphasis,
         ParserTag::Strong => Tag::Strong,
         ParserTag::Strikethrough => Tag::Strikethrough,
-        ParserTag::Link(_, _, _) => Tag::Link,
-        ParserTag::Image(_, _, _) => Tag::Image,
+        ParserTag::Link(link_type, dest_url, title) => Tag::Link(
+            transform_link_type(link_type),
+            dest_url.into_string(),
+            title.into_string(),
+        ),
+        ParserTag::Image(link_type, dest_url, title) => Tag::Image(
+            transform_link_type(link_type),
+            dest_url.into_string(),
+            title.into_string(),
+        ),
+    }
+}
+
+fn transform_link_type(link_type: ParserLinkType) -> LinkType {
+    match link_type {
+        ParserLinkType::Inline => LinkType::Inline,
+        ParserLinkType::Reference => LinkType::Reference,
+        ParserLinkType::ReferenceUnknown => LinkType::Reference,
+        ParserLinkType::Collapsed => LinkType::Collapsed,
+        ParserLinkType::CollapsedUnknown => LinkType::Collapsed,
+        ParserLinkType::Shortcut => LinkType::Shortcut,
+        ParserLinkType::ShortcutUnknown => LinkType::Shortcut,
+        ParserLinkType::Autolink => LinkType::Autolink,
+        ParserLinkType::Email => LinkType::Email,
     }
 }
 
@@ -71,12 +109,14 @@ pub fn markdown_to_event_list(input: String) -> Vec<Event> {
 }
 
 #[cfg(test)]
-pub mod test_markdown_parsing {
+pub mod markdown_parsing {
     use super::*;
     use crate::notes::content_schema::*;
 
+    const TEST_URL: &str = "http://example.com";
+
     #[test]
-    fn test_parsing() {
+    fn parse_to_html() {
         assert_eq!(
             markdown_to_html("# Hello world".to_string()),
             "<h1>Hello world</h1>\n"
@@ -95,15 +135,22 @@ pub mod test_markdown_parsing {
     }
 
     #[test]
-    fn test_events() {
+    fn header_events() {
         assert_eq!(
-            markdown_to_event_list("# Hello world".to_string()),
+            markdown_to_event_list("# Hello\n## world".to_string()),
             vec![
                 Event::Start(Tag::Heading(1)),
-                Event::Text("Hello world".to_string()),
+                Event::Text("Hello".to_string()),
                 Event::End(Tag::Heading(1)),
+                Event::Start(Tag::Heading(2)),
+                Event::Text("world".to_string()),
+                Event::End(Tag::Heading(2)),
             ]
         );
+    }
+
+    #[test]
+    fn strikethrough_events() {
         assert_eq!(
             markdown_to_event_list("~~Hello world~~".to_string()),
             vec![
@@ -114,6 +161,10 @@ pub mod test_markdown_parsing {
                 Event::End(Tag::Paragraph),
             ]
         );
+    }
+
+    #[test]
+    fn task_list_events() {
         assert_eq!(
             markdown_to_event_list("- [ ] hello\n- [ ] world".to_string()),
             vec![
@@ -127,6 +178,73 @@ pub mod test_markdown_parsing {
                 Event::Text("world".to_string()),
                 Event::End(Tag::Item),
                 Event::End(Tag::List(None)),
+            ]
+        );
+    }
+
+    #[test]
+    fn link_events() {
+        assert_eq!(
+            markdown_to_event_list(format!("[hello]({} \"the title\")", TEST_URL).to_string()),
+            vec![
+                Event::Start(Tag::Paragraph),
+                Event::Start(Tag::Link(
+                    LinkType::Inline,
+                    TEST_URL.to_string(),
+                    "the title".to_string()
+                )),
+                Event::Text("hello".to_string()),
+                Event::End(Tag::Link(
+                    LinkType::Inline,
+                    TEST_URL.to_string(),
+                    "the title".to_string()
+                )),
+                Event::End(Tag::Paragraph),
+            ]
+        );
+
+        assert_eq!(
+            markdown_to_event_list(format!("<{}>", TEST_URL).to_string()),
+            vec![
+                Event::Start(Tag::Paragraph),
+                Event::Start(Tag::Link(
+                    LinkType::Autolink,
+                    TEST_URL.to_string(),
+                    "".to_string()
+                )),
+                Event::Text(TEST_URL.to_string()),
+                Event::End(Tag::Link(
+                    LinkType::Autolink,
+                    TEST_URL.to_string(),
+                    "".to_string()
+                )),
+                Event::End(Tag::Paragraph),
+            ]
+        );
+    }
+
+    #[test]
+    fn image_events() {
+        let image_url = format!("{}/image.jpg", TEST_URL);
+        assert_eq!(
+            markdown_to_event_list(format!(
+                "![test image]({} \"the title\")",
+                image_url.to_string()
+            )),
+            vec![
+                Event::Start(Tag::Paragraph),
+                Event::Start(Tag::Image(
+                    LinkType::Inline,
+                    image_url.to_string(),
+                    "the title".to_string()
+                )),
+                Event::Text("test image".to_string()),
+                Event::End(Tag::Image(
+                    LinkType::Inline,
+                    image_url.to_string(),
+                    "the title".to_string()
+                )),
+                Event::End(Tag::Paragraph),
             ]
         );
     }
