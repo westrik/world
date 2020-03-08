f10e43d2bea8fda49333c710e5447681d3166509
Matt Westrik <m@ttwestrik.com>
2020-03-08 13:55:57 -0700

    remove profiling

--- server/core/src/notes/parsing.rs
+++ server/core/src/notes/parsing.rs
@@ -48,18 +48,15 @@ fn alignment_to_column_type(alignment: Alignment) -> ColumnType {
 
 // TODO: refactor this mess
 pub fn markdown_to_elements(content: String) -> Vec<Element> {
-    let pdcm_guard = flame::start_guard("pulldown_cmark");
     let events: Vec<(Event, Range<usize>)> =
         Parser::new_ext(content.as_str(), get_parser_options())
             .into_offset_iter()
             .collect();
-    pdcm_guard.end();
     let mut tag_stack: Vec<Tag> = Vec::new();
     let mut context: Vec<Box<[Element]>> = Vec::new();
     let mut elements: Vec<Element> = Vec::new();
 
     for (event, _) in events {
-        let event_guard = flame::start_guard(format!("event {:?}", event));
         match event {
             Event::Start(tag) => {
                 tag_stack.push(tag);
@@ -189,7 +186,6 @@ pub fn markdown_to_elements(content: String) -> Vec<Element> {
                 });
             }
         }
-        event_guard.end();
     }
 
     elements.into_boxed_slice().into_vec()
@@ -373,23 +369,5 @@ pub mod markdown_parsing {
                 }])
             }]
         );
-    }
-
-    #[test]
-    fn profile__markdown_to_elements() {
-        use flame;
-        use flamescope;
-        use std::fs::File;
-        use std::str::from_utf8;
-
-        static CRDT_BYTES: &[u8] = include_bytes!("../../benches/data/xi-editor-crdt.md");
-        let input = from_utf8(CRDT_BYTES).unwrap();
-
-        let main_guard = flame::start_guard("main");
-
-        markdown_to_elements(input.to_string());
-
-        main_guard.end();
-        flamescope::dump(&mut File::create("flamescope.json").unwrap()).unwrap();
     }
 }
