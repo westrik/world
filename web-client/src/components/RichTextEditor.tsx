b840aa555b21955718b0b67018a29315a83ae2fb
Matt Westrik <m@ttwestrik.com>
2020-03-12 01:42:59 -0700

    refactoring more

--- web-client/src/components/RichTextEditor.tsx
+++ web-client/src/components/RichTextEditor.tsx
@@ -8,16 +8,9 @@ import { ContentElement } from '~/notes/ContentElement';
 
 export default function RichTextEditor(): h.JSX.Element {
     const [content, setContent] = useState<Content>(NOTE_FIXTURE.content!);
-    return <ContentArea content={content} />;
-}
-
-interface ContentAreaProps {
-    content: Content;
-}
-export function ContentArea(props: ContentAreaProps): h.JSX.Element {
     return (
         <div>
-            {props.content.elements.map((el: Element, key: number) => (
+            {content.elements.map((el: Element, key: number) => (
                 <ContentElement key={key} element={el} />
             ))}
         </div>
