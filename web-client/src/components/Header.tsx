2d107845ad7ecf24e384001a657fe726b7594966
Matt Westrik <m@ttwestrik.com>
2020-03-16 20:58:13 -0700

    [fe] tweak header back-link styling

--- web-client/src/components/Header.tsx
+++ web-client/src/components/Header.tsx
@@ -18,14 +18,16 @@ export default function Header(props: Props): h.JSX.Element {
             }
             className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
         >
-            <h1 className="h2">
+            <div>
                 {props.backLink ? (
                     <a className="back-link" href={props.backLink}>
                         &larr; {props.backLinkTitle ? props.backLinkTitle : 'back'}
                     </a>
                 ) : null}
-                {props.title}
-            </h1>
+                <h1 className="h2" style="display: inline-block;">
+                    {props.title}
+                </h1>
+            </div>
             {props.children ? (
                 <div className="form-inline">
                     {props.children.map((child, key) => (
