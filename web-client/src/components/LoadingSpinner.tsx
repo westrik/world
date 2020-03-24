b903083c4dcf430179d78d48a7d24ee572916dfa
Matt Westrik <m@ttwestrik.com>
2020-03-23 18:55:41 -0700

    [fe] add css loading spinner

--- web-client/src/components/LoadingSpinner.tsx
+++ web-client/src/components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import { h } from 'preact';
 
 export default function LoadingSpinner(): h.JSX.Element {
     return (
-        <div className="spinner-border mx-auto" role="status">
+        <div className="loading-spinner" role="status">
             <span className="sr-only">Loading...</span>
         </div>
     );
