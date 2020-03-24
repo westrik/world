202c0bc037a809108fc650438543d0c3de09f529
Matt Westrik <m@ttwestrik.com>
2020-03-23 18:19:06 -0700

    [fe] improve nav layout

--- web-client/src/components/ListContainer.tsx
+++ web-client/src/components/ListContainer.tsx
@@ -1,5 +1,5 @@
 import { h } from 'preact';
 
 export default function ListContainer({ children }: { children: Array<h.JSX.Element> }): h.JSX.Element {
-    return <ul style="list-style: none; padding: 0; padding-bottom: 4em; padding-top: 6em;">{children}</ul>;
+    return <ul className="list-container">{children}</ul>;
 }
