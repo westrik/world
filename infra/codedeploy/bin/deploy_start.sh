6a6969b7a225ad6dc66dab0816d226c5c38c76e7
Matt Westrik <m@ttwestrik.com>
2020-04-10 19:15:04 -0700

    fix other systemd env vars also

--- infra/codedeploy/bin/deploy_start.sh
+++ infra/codedeploy/bin/deploy_start.sh
@@ -30,9 +30,9 @@ RDS_PASSWORD=$(aws secretsmanager get-secret-value --secret-id "westrikworld_dat
 RDS_DB_NAME=$(aws secretsmanager get-secret-value --secret-id "westrikworld_database_name" | jq -r '.SecretString')
 PASSWORD_HASH_SALT=$(aws secretsmanager get-secret-value --secret-id "westrikworld_password_hash_salt" | jq -r '.SecretString')
 echo "[Service]" >> $SECRETS_ENV_FILE
-echo "Environment=\"DATABASE_URL=postgres://$RDS_USER:$RDS_PASSWORD@$RDS_HOST/$RDS_DB_NAME\"" >> $SECRETS_ENV_FILE
+echo "Environment=\"DATABASE_URL='postgres://$RDS_USER:$RDS_PASSWORD@$RDS_HOST/$RDS_DB_NAME'\"" >> $SECRETS_ENV_FILE
 echo "Environment=\"PASSWORD_HASH_SALT='$PASSWORD_HASH_SALT'\"" >> $SECRETS_ENV_FILE
-echo "Environment=\"CORS_ORIGIN_URL=https://westrikworld.com\"" >> $SECRETS_ENV_FILE
+echo "Environment=\"CORS_ORIGIN_URL='https://westrikworld.com'\"" >> $SECRETS_ENV_FILE
 systemctl daemon-reload
 
 chown root:root $SERVER_BIN_FILE
