7e1e1c2746a976d1886e3d1cf32a012142e376e6
Matt Westrik <m@ttwestrik.com>
2020-05-14 01:09:09 -0700

    hmm maybe use lambda instead

--- infra/lambda/deploy_lambdas.sh
+++ infra/lambda/deploy_lambdas.sh
@@ -7,21 +7,30 @@ if [ -z "$PROJECT_ROOT" ]; then
 fi
 set -u
 
+cd "$PROJECT_ROOT/infra/lambda"
+
 # Note: this script assumes that lambda names won't include spaces
 LAMBDAS=(
   create_db_user_with_iam_role
   renew_certificate
 )
 
-# TODO: spin up EC2 instance
+# TODO: spin up EC2 instance w/ SSH key
 # OQ: where does the VPC ID come from?
+# - requires :22 to be open OR needs an additional bastion instance
+# OQ: can this be a lambda instead?????
+# - more secure (no SSH), faster to deploy
 
 for lambda in "${LAMBDAS[@]}"; do
   echo "Deploying $lambda";
   SOURCE_BUNDLE="$lambda.tar.gz"
   tar -czf "$SOURCE_BUNDLE" "$lambda"
+
+  # TODO: use AWS CLI to trigger deploy lambda
+
+  # - OR: -
+
   # TODO: scp bundle to the EC2 instance
-  rm -f "$SOURCE_BUNDLE"
   # TODO: run deploy script for this lambda
   #  first:
   #    - echo lambda name
@@ -30,6 +39,8 @@ for lambda in "${LAMBDAS[@]}"; do
   #    - pip install
   #    - tar up the whole bundle
   #    - send the bundle to S3
+
+  rm -f "$SOURCE_BUNDLE"
 done
 
 # TODO: kill the instance
