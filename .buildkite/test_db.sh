b63c3ed93c1a0c55726b5e4efb99a29a5051aee4
Matt Westrik <m@ttwestrik.com>
2020-05-18 03:33:20 -0700

    buildkite scripts directory

---
+++ .buildkite/test_db.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+sed -i 's/local   all             postgres                                peer/host    all             all             127.0.0.1\/32            trust/g' /etc/postgresql/10/main/pg_hba.conf
+service postgresql restart
+apt-get install -y sudo
+sudo -u postgres psql postgres -w -h localhost -c 'create database westrikworld_test;'
+sudo -u postgres psql postgres -w -h localhost -c "create user westrikworld_user with encrypted password 'PASSWORD';"
+sudo -u postgres psql postgres -w -h localhost -c "grant all privileges on database westrikworld_test to westrikworld_user;"
+touch .env
+echo "TEST_DATABASE_URL='postgres://westrikworld_user:PASSWORD@localhost/westrikworld_test'" >> .env
+echo "PASSWORD_HASH_SALT='lBi5Pi6yBdtRmIXMZvs6IlmAOy3ogEivqG056hGJ6t'" >> .env
