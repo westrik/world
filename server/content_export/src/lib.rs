ca02addea7df3e36e9f600c1f5b4047a0b1b5b90
Matt Westrik <m@ttwestrik.com>
2020-06-22 21:56:28 -0700

    [SQUASH ME]

--- server/content_export/src/lib.rs
+++ server/content_export/src/lib.rs
@@ -2,17 +2,29 @@ use std::fs::File;
 use std::io::Write;
 use tectonic;
 
-pub fn content_to_pdf(content: &str, output: Option<&str>) {
+pub fn content_to_pdf(content: &str, output_file_path: Option<&str>) {
     // TODO: convert markdown content to latex
     let pdf_data: Vec<u8> = tectonic::latex_to_pdf(content).expect("processing failed");
     println!("Output PDF size is {} bytes.", pdf_data.len());
-    if let Some(output_path) = output {
+    if let Some(output_path) = output_file_path {
         println!("Writing to {}...", &output_path);
         // TODO: improve error handling
         let mut file = File::create(&output_path).unwrap();
         file.write_all(&pdf_data).unwrap();
     }
 }
+
+// fn latex_to_dvi(content: &str, output_file_path: Option<&str>) {
+//     // TODO: use OutputFormat::Xdv
+// }
+
+// fn dvi_to_svg(content: &Vec<u8>, output_file_path: Option<&str>) {
+//     // TODO: shell out to dvisvgm (https://dvisvgm.de/Manpage/)
+// }
+
+// fn upload_svg_to_s3(content: &Vec<u8>) {
+//
+// }
 
 #[cfg(test)]
 pub mod content_export {
