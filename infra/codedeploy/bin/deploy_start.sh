7fd0e53c632c050059594db6deb86a8debf8d06e
Matt Westrik <m@ttwestrik.com>
2020-07-05 17:54:48 -0700

    swap some lines

--- infra/codedeploy/bin/deploy_start.sh
+++ infra/codedeploy/bin/deploy_start.sh
@@ -4,13 +4,13 @@ set -euxo pipefail
 
 SERVER_BIN_FILE=/usr/bin/api_server
 
+chown root:root $SERVER_BIN_FILE
+chmod 755 $SERVER_BIN_FILE
+
 systemctl daemon-reload
 systemctl enable secrets.service secrets.target secrets.timer
 systemctl enable app.service app.target
 systemctl enable nginx.service
-
-chown root:root $SERVER_BIN_FILE
-chmod 755 $SERVER_BIN_FILE
 
 systemctl restart secrets app nginx
 
