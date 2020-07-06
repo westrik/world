462970351162be955366678a520dc7d1fb23de41
Matt Westrik <m@ttwestrik.com>
2020-07-06 01:30:28 -0700

    remove block fixtures

--- integration_tests/src/fixtures/mod.rs
+++ integration_tests/src/fixtures/mod.rs
@@ -1,5 +1,3 @@
 pub mod auth;
-pub mod blocks;
 
 pub use auth::*;
-pub use blocks::*;
--- integration_tests/src/tests/db_tasks.rs
+++ integration_tests/src/tests/db_tasks.rs
@@ -1,19 +1,16 @@
-use crate::db::{get_conn, DbConnection, DbPool};
-use crate::fixtures::auth::{get_test_session, get_test_user};
-// use crate::fixtures::blocks::create_test_block;
-use westrikworld_core::content::models::task::{Task, TaskCreateSpec};
+use crate::db::DbConnection;
+use westrikworld_core::content::models::task::TaskCreateSpec;
 use westrikworld_core::resource_identifier::{generate_resource_identifier, ResourceType};
 
 // TODO:
 //  - test update logic
 //  - test parent-child logic
 
-fn create_n_tasks(conn: &DbConnection, n: i32, user_id: i32, block_id: i32) {
+pub fn create_n_tasks(conn: &DbConnection, n: i32, user_id: i32) {
     for id in 0..n {
         let new_task = TaskCreateSpec {
             api_id: generate_resource_identifier(ResourceType::Task),
             user_id,
-            block_id,
             description: format!("HELLO WORLD #{:04}", id).to_string(),
         };
         new_task.insert(conn).unwrap();
