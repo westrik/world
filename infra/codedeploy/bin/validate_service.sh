805b5178cfd747a16f2311e2df151ace01c36d62
Matt Westrik <m@ttwestrik.com>
2020-07-05 18:48:04 -0700

    debugging validate script

--- infra/codedeploy/bin/validate_service.sh
+++ infra/codedeploy/bin/validate_service.sh
@@ -8,7 +8,8 @@ INITIAL_SLEEP_TIME=10
 REQUEST_SLEEP_TIME=2
 
 function test_service() {
-  curl --silent --output /dev/null --write-out "%{http_code}" "$1"
+  # curl --silent --output /dev/null --write-out "%{http_code}" "$1"
+  curl -v "$1"
 }
 
 echo "waiting $INITIAL_SLEEP_TIME sec for services to start"
@@ -16,8 +17,8 @@ sleep $INITIAL_SLEEP_TIME
 
 for i in $(seq 1 $TEST_ITERATIONS)
 do
-  status_code=$(test_service $TEST_URL)
   printf "making a request to %s... " "$TEST_URL"
+  status_code=$(test_service $TEST_URL)
   if [[ status_code -eq 200 ]] ; then
     echo "succeeded"
     exit 0
