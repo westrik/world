ae842e8293cda90f1d753805fdf514afe5abf63f
Matt Westrik <m@ttwestrik.com>
2020-07-05 23:03:42 -0700

    revert some stuff

--- infra/modules/core_infra/network/security_groups/main.tf
+++ infra/modules/core_infra/network/security_groups/main.tf
@@ -9,13 +9,13 @@ resource "aws_security_group" "app_inbound" {
   description = "[${var.project_slug}-${var.deploy_name}] inbound app instance security group"
   vpc_id      = var.vpc_id
 
-  # Inbound SSH (TODO: disable)
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  //  # Inbound SSH (TODO: disable)
+  //  ingress {
+  //    from_port   = 22
+  //    to_port     = 22
+  //    protocol    = "tcp"
+  //    cidr_blocks = ["0.0.0.0/0"]
+  //  }
 
   # Inbound HTTP via NLB
   ingress {
