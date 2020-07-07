9d95d0eac34876e2151088fef45bac9d5dc1bbd9
Matt Westrik <m@ttwestrik.com>
2020-07-06 19:47:34 -0700

    add colors from other libraries

---
+++ web-client/src/components/previews/ComponentLibraryPreview.tsx
@@ -0,0 +1,5 @@
+import {h} from "preact";
+
+export default function ComponentLibraryPreview(): h.JSX.Element {
+    return <div>Hello world!</div>
+}
---
+++ web-client/src/components/previews/Swatch.tsx
@@ -0,0 +1,9 @@
+import {h} from "preact";
+
+interface SwatchProps {
+
+}
+
+export default function Swatch(props: SwatchProps): h.JSX.Element {
+    return <div></div>
+}
