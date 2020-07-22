f3f23e4e34e51d98c14e5e79c8346a4efd3ac609
Matt Westrik <m@ttwestrik.com>
2020-07-21 19:55:42 -0700

    [be] refactor test crate

---
+++ tests/src/tests/api/mod.rs
@@ -0,0 +1,8 @@
+//  TODO:
+// pub mod auth;
+//  TODO:
+// pub mod library_items;
+//  TODO:
+// pub mod notes;
+//  TODO:
+// pub mod tasks;
--- tests/src/tests/mod.rs
+++ tests/src/tests/mod.rs
@@ -1,5 +1,2 @@
-mod api_tasks;
-
-mod db_auth;
-mod db_tasks;
-mod job_subscription;
+pub mod api;
+pub mod db;
