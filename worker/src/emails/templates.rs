9362feebfe47aeb52e3448d766a18d8188a7d61a
Matt Westrik <m@ttwestrik.com>
2020-08-04 19:42:12 -0700

    ?????

--- worker/src/emails/templates.rs
+++ worker/src/emails/templates.rs
@@ -1,14 +1,36 @@
-use tera::Tera;
+use tera::{Context, Tera};
 use world_core::jobs::errors::JobError;
 
+// lazy_static! {
+//     pub static ref TEMPLATES: Tera = {
+//         tera
+//     };
+// }
+//
+// // "login_notification"
+//
+
 pub fn populate_email_template() -> Result<String, JobError> {
-    let _templates = match Tera::new("templates/**/*.html") {
-        Ok(t) => Ok(t),
-        Err(_) => Err(JobError::InternalError(
-            "Could not find email templates".to_string(),
-        )),
-    }?;
-    Ok("TEMPLATE".to_string())
+    // TODO: render HTML and text
+
+    let mut tera = match Tera::new("./templateasdioafdsikljadfskljs/**/*") {
+        Ok(t) => t,
+        Err(e) => panic!("Template parsing error(s): {}", e),
+    };
+    // if let Err(err) = tera.add_template_files(vec![
+    //     ("login_notification.html", Some("login_notification")),
+    //     ("login_notification.txt", Some("login_notification_txt")),
+    // ]) {
+    //     panic!("Failed to load template(s): {} [dir={:#?}]", err, std::env::current_dir().unwrap());
+    //
+    // }
+    let mut context = Context::new();
+    context.insert("site_name", "westrikworld");
+    let rendered = tera
+        .render("login_notification.html", &context)
+        .map_err(|e| JobError::InternalError(format!("Failed to render template: {}", e)))?;
+
+    Ok(rendered.to_string())
 }
 
 #[cfg(test)]
