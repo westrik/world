254f31354eba74ca84e9017e292900f5a2936d13
Matt Westrik <m@ttwestrik.com>
2020-08-28 00:04:48 -0700

    refactor utils::configs to use macro_rules!

--- core/src/utils/config.rs
+++ core/src/utils/config.rs
@@ -1,13 +1,22 @@
 use std::env;
 
-lazy_static! {
-    pub static ref CLOUDFRONT_KEYPAIR_ID: String = env::var("CLOUDFRONT_KEYPAIR_ID").expect("CLOUDFRONT_KEYPAIR_ID must be set");
-    pub static ref CLOUDFRONT_PRIVATE_KEY: String = env::var("CLOUDFRONT_PRIVATE_KEY").expect("CLOUDFRONT_PRIVATE_KEY must be set");
-    pub static ref CONTENT_BUCKET_NAME: String = env::var("CONTENT_BUCKET_NAME").expect("CONTENT_BUCKET_NAME must be set");
-    pub static ref IAM_ROLE_ARN: String = env::var("IAM_ROLE_ARN").expect("IAM_ROLE_ARN must be set");
-    pub static ref OUTBOUND_EMAIL_SENDER: String = env::var("OUTBOUND_EMAIL_SENDER").expect("OUTBOUND_EMAIL_SENDER must be set");
-    pub static ref PASSWORD_HASH_SALT: String = env::var("PASSWORD_HASH_SALT").expect("PASSWORD_HASH_SALT must be set");
-    pub static ref ROOT_DOMAIN_NAME: String = env::var("ROOT_DOMAIN_NAME").expect("ROOT_DOMAIN_NAME must be set");
-    pub static ref SENDGRID_API_KEY: String = env::var("SENDGRID_API_KEY").expect("SENDGRID_API_KEY must be set");
-    // pub static ref SERVICE_PROXY_LAMBDA_ARN: String = env::var("SERVICE_PROXY_LAMBDA_ARN").expect("SERVICE_PROXY_LAMBDA_ARN must be set");
+macro_rules! config_flag {
+    ($flag_name:ident) => {
+        lazy_static! {
+            pub static ref $flag_name: String = env::var(stringify!($flag_name)).expect(&format!(
+                "Expected environment variable '{}' to be set",
+                stringify!($flag_name)
+            ));
+        }
+    };
 }
+
+config_flag!(CLOUDFRONT_KEYPAIR_ID);
+config_flag!(CLOUDFRONT_PRIVATE_KEY);
+config_flag!(CONTENT_BUCKET_NAME);
+config_flag!(IAM_ROLE_ARN);
+config_flag!(OUTBOUND_EMAIL_SENDER);
+config_flag!(PASSWORD_HASH_SALT);
+config_flag!(ROOT_DOMAIN_NAME);
+config_flag!(SENDGRID_API_KEY);
+config_flag!(SERVICE_PROXY_LAMBDA_ARN);
