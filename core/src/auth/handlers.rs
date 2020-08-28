6c081ebd91d0310f7f0027c3dadf53db22a52f83
Matt Westrik <m@ttwestrik.com>
2020-08-28 01:13:37 -0700

    set SameSite=None to allow cross-origin requests

--- core/src/auth/handlers.rs
+++ core/src/auth/handlers.rs
@@ -109,7 +109,7 @@ pub async fn cloudfront_authenticate(
     let mut response_builder = Response::builder();
     for header in cookie_headers {
         let value = HeaderValue::from_str(&format!(
-            "{}={}; Domain={}; Path={}; Secure; HttpOnly",
+            "{}={}; Domain={}; Path={}; Secure; HttpOnly; SameSite=None",
             header.0, header.1, domain, path
         ))
         .map_err(|_| ApiError::InternalError("Could not create CloudFront cookie".to_string()))?;
