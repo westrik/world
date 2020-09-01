981b8a680c5c50216fd1600691581da10d0c75dc
Matt Westrik <m@ttwestrik.com>
2020-08-31 23:05:07 -0700

    samesite=strict

--- core/src/auth/handlers.rs
+++ core/src/auth/handlers.rs
@@ -113,7 +113,7 @@ pub async fn cloudfront_authenticate(
     let mut response_builder = Response::builder();
     for header in cookie_headers {
         let value = HeaderValue::from_str(&format!(
-            "{}={}; Domain={}; Path={}; Secure; SameSite=None; HttpOnly",
+            "{}={}; Domain={}; Path={}; Secure; SameSite=Strict; HttpOnly",
             header.0, header.1, *ROOT_DOMAIN_NAME, cloudfront_access.path
         ))
         .map_err(|_| ApiError::InternalError("Could not create CloudFront cookie".to_string()))?;
