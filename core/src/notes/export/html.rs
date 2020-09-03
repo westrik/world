1a94d2ca9d289736195bc26ffd0c342dab86b125
Matt Westrik <m@ttwestrik.com>
2020-09-03 00:28:24 -0700

    fix clippy

--- core/src/notes/export/html.rs
+++ core/src/notes/export/html.rs
@@ -25,80 +25,80 @@ impl Render<Html> for Content {
 
 impl Render<Html> for Element {
     fn render(&self) -> Html {
-        match &self.element {
-            // ElementType::Text(str) => Html {
-            //     output: str.to_string(),
-            // },
-            // ElementType::Code(str) => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::Html(_) => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::Paragraph => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::Emphasis => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::Strong => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::Strikethrough => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::Heading(heading_type) => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::Link(link_data) => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::Image(link_data) => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::CodeBlock(code_block_data) => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::List(list_data) => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::Item => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::TaskListMarker(marker_data) => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::BlockQuote => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::FootnoteDefinition(str) => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::FootnoteReference(str) => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::Table(table_data) => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::TableHead => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::TableRow => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::TableCell => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::SoftBreak => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::HardBreak => Html {
-            //     output: "".to_string(),
-            // },
-            // ElementType::Rule => Html {
-            //     output: "".to_string(),
-            // },
-            _ => unimplemented!(),
-        }
+        // match &self.element {
+        //     ElementType::Text(str) => Html {
+        //         output: str.to_string(),
+        //     },
+        //     ElementType::Code(str) => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::Html(_) => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::Paragraph => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::Emphasis => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::Strong => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::Strikethrough => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::Heading(heading_type) => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::Link(link_data) => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::Image(link_data) => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::CodeBlock(code_block_data) => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::List(list_data) => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::Item => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::TaskListMarker(marker_data) => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::BlockQuote => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::FootnoteDefinition(str) => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::FootnoteReference(str) => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::Table(table_data) => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::TableHead => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::TableRow => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::TableCell => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::SoftBreak => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::HardBreak => Html {
+        //         output: "".to_string(),
+        //     },
+        //     ElementType::Rule => Html {
+        //         output: "".to_string(),
+        //     },
+        // }
+        unimplemented!()
     }
 }
