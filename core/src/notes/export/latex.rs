60a4e70d923fd1aeab271653ca3b63b17e3d492a
Matt Westrik <m@ttwestrik.com>
2020-09-02 23:51:37 -0700

    fix build (but not tests)

--- core/src/notes/export/latex.rs
+++ core/src/notes/export/latex.rs
@@ -1,6 +1,6 @@
 use crate::notes::{
     export::Render,
-    schema::{Content, Element, ElementType},
+    schema::{Content, Element},
 };
 
 pub struct Latex {
@@ -26,78 +26,79 @@ impl Render<Latex> for Content {
 impl Render<Latex> for Element {
     fn render(&self) -> Latex {
         match &self.element {
-            ElementType::Text(str) => Latex {
-                output: str.to_string(),
-            },
-            ElementType::Code(str) => Latex {
-                output: "".to_string(),
-            },
-            ElementType::Html(_) => Latex {
-                output: "".to_string(),
-            },
-            ElementType::Paragraph => Latex {
-                output: "".to_string(),
-            },
-            ElementType::Emphasis => Latex {
-                output: "".to_string(),
-            },
-            ElementType::Strong => Latex {
-                output: "".to_string(),
-            },
-            ElementType::Strikethrough => Latex {
-                output: "".to_string(),
-            },
-            ElementType::Heading(heading_type) => Latex {
-                output: "".to_string(),
-            },
-            ElementType::Link(link_data) => Latex {
-                output: "".to_string(),
-            },
-            ElementType::Image(link_data) => Latex {
-                output: "".to_string(),
-            },
-            ElementType::CodeBlock(code_block_data) => Latex {
-                output: "".to_string(),
-            },
-            ElementType::List(list_data) => Latex {
-                output: "".to_string(),
-            },
-            ElementType::Item => Latex {
-                output: "".to_string(),
-            },
-            ElementType::TaskListMarker(marker_data) => Latex {
-                output: "".to_string(),
-            },
-            ElementType::BlockQuote => Latex {
-                output: "".to_string(),
-            },
-            ElementType::FootnoteDefinition(str) => Latex {
-                output: "".to_string(),
-            },
-            ElementType::FootnoteReference(str) => Latex {
-                output: "".to_string(),
-            },
-            ElementType::Table(table_data) => Latex {
-                output: "".to_string(),
-            },
-            ElementType::TableHead => Latex {
-                output: "".to_string(),
-            },
-            ElementType::TableRow => Latex {
-                output: "".to_string(),
-            },
-            ElementType::TableCell => Latex {
-                output: "".to_string(),
-            },
-            ElementType::SoftBreak => Latex {
-                output: "".to_string(),
-            },
-            ElementType::HardBreak => Latex {
-                output: "".to_string(),
-            },
-            ElementType::Rule => Latex {
-                output: "".to_string(),
-            },
+            // ElementType::Text(str) => Latex {
+            //     output: str.to_string(),
+            // },
+            // ElementType::Code(str) => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Html(_) => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Paragraph => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Emphasis => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Strong => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Strikethrough => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Heading(heading_type) => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Link(link_data) => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Image(link_data) => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::CodeBlock(code_block_data) => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::List(list_data) => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Item => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::TaskListMarker(marker_data) => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::BlockQuote => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::FootnoteDefinition(str) => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::FootnoteReference(str) => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Table(table_data) => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::TableHead => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::TableRow => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::TableCell => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::SoftBreak => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::HardBreak => Latex {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Rule => Latex {
+            //     output: "".to_string(),
+            // },
+            _ => unimplemented!(),
         }
     }
 }
--- core/src/notes/export/markdown.rs
+++ core/src/notes/export/markdown.rs
@@ -24,78 +24,79 @@ impl Render<Markdown> for Content {
 impl Render<Markdown> for Element {
     fn render(&self) -> Markdown {
         match &self.element {
-            ElementType::Text(str) => Markdown {
-                output: str.to_string(),
-            },
-            ElementType::Code(str) => Markdown {
-                output: format!("`{}`", str),
-            },
-            ElementType::Html(_) => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::Paragraph => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::Emphasis => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::Strong => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::Strikethrough => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::Heading(heading_type) => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::Link(link_data) => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::Image(link_data) => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::CodeBlock(code_block_data) => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::List(list_data) => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::Item => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::TaskListMarker(marker_data) => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::BlockQuote => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::FootnoteDefinition(str) => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::FootnoteReference(str) => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::Table(table_data) => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::TableHead => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::TableRow => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::TableCell => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::SoftBreak => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::HardBreak => Markdown {
-                output: "".to_string(),
-            },
-            ElementType::Rule => Markdown {
-                output: "".to_string(),
-            },
+            // ElementType::Text(str) => Markdown {
+            //     output: str.to_string(),
+            // },
+            // ElementType::Code(str) => Markdown {
+            //     output: format!("`{}`", str),
+            // },
+            // ElementType::Html(_) => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Paragraph => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Emphasis => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Strong => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Strikethrough => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Heading(heading_type) => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Link(link_data) => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Image(link_data) => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::CodeBlock(code_block_data) => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::List(list_data) => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Item => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::TaskListMarker(marker_data) => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::BlockQuote => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::FootnoteDefinition(str) => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::FootnoteReference(str) => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Table(table_data) => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::TableHead => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::TableRow => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::TableCell => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::SoftBreak => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::HardBreak => Markdown {
+            //     output: "".to_string(),
+            // },
+            // ElementType::Rule => Markdown {
+            //     output: "".to_string(),
+            // },
+            _ => unimplemented!(),
         }
     }
 }
