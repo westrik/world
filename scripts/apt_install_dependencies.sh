cb5165ed6396321b8919f9ab1b533ff2d88204c4
Matt Westrik <m@ttwestrik.com>
2020-09-07 01:10:29 -0700

    upgrade postgres (11->12)

--- scripts/apt_install_dependencies.sh
+++ scripts/apt_install_dependencies.sh
@@ -6,14 +6,16 @@ apt-get update
 # general system dependencies
 apt-get install -y sudo curl
 # other toolchain dependencies
-apt-get install -y build-essential
+apt-get install -y build-essential lsb-release
 # OpenSSL (needed for postgres and tectonic)
 apt-get install -y libssl-dev
-# postgres dependencies (needed to build diesel w/ pg enabled)
-apt-get install -y libpq-dev postgresql postgresql-client
+# postgres 12 (needed to build diesel w/ pg enabled)
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt-get update
+apt-get install -y postgresql-12 postgresql-client-12
 # dependencies used by tectonic (crate used for PDF generation)
 apt-get -y install libfontconfig1-dev libgraphite2-dev libharfbuzz-dev libicu-dev zlib1g-dev
-
 
 ## install rust toolchain
 if ! command -v rustup >/dev/null 2>&1; then
