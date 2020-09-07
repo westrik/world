2c018db4462589ba3dc16051bd3e5bc4f36067c5
Matt Westrik <m@ttwestrik.com>
2020-09-07 03:28:47 -0700

    install libpq-dev

--- scripts/apt_install_dependencies.sh
+++ scripts/apt_install_dependencies.sh
@@ -31,4 +31,5 @@ curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update
 apt-get install -y \
     postgresql-12 \
-    postgresql-client-12
+    postgresql-client-12 \
+    libpq-dev
