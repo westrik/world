290736713bceff6ed3a0c7a31cd73840dd8be17d
Matt Westrik <m@ttwestrik.com>
2020-09-07 01:40:49 -0700

    add pg_ctl reload

--- scripts/setup_database.sh
+++ scripts/setup_database.sh
@@ -59,9 +59,10 @@ if [[ "$OSTYPE" != "darwin"* ]]; then
   cat /etc/postgresql/12/main/pg_hba.conf
   sudo sed -i 's/local   all             postgres                                peer/host    all             all             127.0.0.1\/32            trust/g' /etc/postgresql/12/main/pg_hba.conf
   cat /etc/postgresql/12/main/pg_hba.conf
+  echo "reloading postgres config"
+  pg_ctl reload
   echo "restarting postgres..."
   service postgresql restart
-  echo "restarted"
 fi
 
 db_name="world_app"
