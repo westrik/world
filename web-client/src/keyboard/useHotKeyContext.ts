0134b2c5e1bb98f20899763a8c3249e164da57ff
Matt Westrik <m@ttwestrik.com>
2020-10-13 22:08:05 -0700

    Update web-client/src/keyboard/useHotKeyContext.ts

--- web-client/src/keyboard/useHotKeyContext.ts
+++ web-client/src/keyboard/useHotKeyContext.ts
@@ -34,8 +34,8 @@ function createKeyDownHandler(ctx: HotKeyContext): (event: KeyboardEvent) => voi
                 })
             ];
         if (handler) {
-            handler();
             event.stopPropagation();
+            handler();            
         }
     };
 }
