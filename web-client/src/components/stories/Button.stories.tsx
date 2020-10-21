4c57da9d4cd9e3d3d4e51726d09860e74d13dc4a
Matt Westrik <m@ttwestrik.com>
2020-10-21 02:16:36 -0700

    feat: add nav sidebar component (#286)
    * add nav sidebar component
    
    * add new sidebar to app container
    
    * fix some broken stuff

--- web-client/src/components/stories/Button.stories.tsx
+++ web-client/src/components/stories/Button.stories.tsx
@@ -1,69 +1,49 @@
 import { h } from 'preact';
 import Button, { ButtonSize, ButtonVariant } from '~components/Button';
-import Popover from '~components/Popover';
 
 export default { title: 'Button' };
 
 export function normal(): h.JSX.Element {
     return (
         <ul className="story-container">
-            <li>
-                <Button variant={ButtonVariant.PRIMARY} size={ButtonSize.LARGE} title="Test Button (Large, Primary)" />
-            </li>
-            <li>
-                <Button title="Test Button (Medium, Secondary)" />
-            </li>
-            <li>
-                <Button variant={ButtonVariant.TERTIARY} title="Test Button (Medium, Tertiary)" />
-            </li>
-            <li>
-                <Button size={ButtonSize.SMALL} title="Test Button (Small, Secondary)" />
-            </li>
-            <li>
-                <Button size={ButtonSize.XSMALL} title="Test Button (X-Small, Secondary)" />
-            </li>
-            <li>
-                <Button
-                    default={true}
-                    disabled={true}
-                    size={ButtonSize.LARGE}
-                    title="Test Button (Large, Default, Disabled)"
-                />
-            </li>
-            <li>
-                <Button default={true} title="Test Button (Medium, Default)" />
-            </li>
-            <li>
-                <Button disabled={true} size={ButtonSize.SMALL} title="Test Button (Small, Disabled)" />
-            </li>
-            <li>
-                <Button disabled={true} size={ButtonSize.XSMALL} title="Test Button (X-Small, Disabled)" />
-            </li>
+            {[ButtonVariant.PRIMARY, ButtonVariant.SECONDARY, ButtonVariant.TERTIARY].map((variant, key) => {
+                {
+                    return [ButtonSize.LARGE, ButtonSize.MEDIUM, ButtonSize.SMALL, ButtonSize.XSMALL].map(
+                        (size, key2) => {
+                            return (
+                                <li key={key * key2}>
+                                    <Button variant={variant} size={size} title={`Test Button (${variant}, ${size})`} />
+                                </li>
+                            );
+                        },
+                    );
+                }
+            })}
         </ul>
     );
 }
 
-export function withPopover(): h.JSX.Element {
-    const showPopper = (): void => {
-        console.log('TODO: show popper');
-    };
-    return (
-        <div className="story-container">
-            <div>
-                <Popover key="popover" />
-            </div>
-            <div>
-                <Button onClick={showPopper} size={ButtonSize.LARGE} title="large" />
-            </div>
-            <div>
-                <Button onClick={showPopper} title="default" />
-            </div>
-            <div>
-                <Button onClick={showPopper} size={ButtonSize.SMALL} title="small" />
-            </div>
-            <div>
-                <Button onClick={showPopper} size={ButtonSize.XSMALL} title="x-small" />
-            </div>
-        </div>
-    );
-}
+// export function withPopover(): h.JSX.Element {
+//     const showPopper = (): void => {
+//         console.log('TODO: show popper');
+//     };
+//     return (
+//         <div className="story-container">
+//             <div>
+//                 <Popover key="popover" />
+//             </div>
+//             <div>
+//                 <Button onClick={showPopper} size={ButtonSize.LARGE} title="large" />
+//             </div>
+//             <div>
+//                 <Button onClick={showPopper} title="default" />
+//             </div>
+//             <div>
+//                 <Button onClick={showPopper} size={ButtonSize.SMALL} title="small" />
+//             </div>
+//             <div>
+//                 <Button onClick={showPopper} size={ButtonSize.XSMALL} title="x-small" />
+//             </div>
+//         </div>
+//     );
+// }
---
+++ web-client/src/components/stories/NavSidebar.stories.tsx
@@ -0,0 +1,8 @@
+import { h } from 'preact';
+import NavSidebar from '~components/NavSidebar';
+
+export default { title: 'Nav Sidebar' };
+
+export function normal(): h.JSX.Element {
+    return <NavSidebar />;
+}
