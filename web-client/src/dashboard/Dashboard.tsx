09ef14521c265dc413137f159d12c4a90ce1e978
Matt Westrik <m@ttwestrik.com>
2020-12-03 17:42:35 -0800

    add search field to dashboard

--- web-client/src/dashboard/Dashboard.tsx
+++ web-client/src/dashboard/Dashboard.tsx
@@ -1,11 +1,20 @@
 import { h } from 'preact';
 
 import AppContainer from '~components/AppContainer';
+import { TextField } from '~components/InputFields';
 
 export default function Dashboard(): h.JSX.Element {
     return (
         <AppContainer>
-            <span>dashboard goes here</span>
+            <TextField
+                labelText="Search"
+                onChange={() => {
+                    console.log('updated');
+                }}
+            />
+            {/* TODO: task summary */}
+            {/* TODO: recent notes */}
+            {/* TODO: IoT stats */}
         </AppContainer>
     );
 }
